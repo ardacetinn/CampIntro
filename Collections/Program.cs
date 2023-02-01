7d129cd0cf16a08406efed8c43df625da82c16cf
ardac <ardacetinn2@gmail.com>
2023-02-01 22:15:15 +0300

    25.01.23

---
+++ Collections/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //string[] names = new string[] 
+            //{
+            //    "Engin",
+            //    "Murat",
+            //    "Kerem",
+            //    "Halil"
+            //};
+
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            List<string> names2 = new List<string> 
+            {
+                "Engin",
+                "Murat",
+                "Kerem",
+                "Halil"
+            };
+
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+
+            names2.Add("Arda");
+            Console.WriteLine(names2[4]);
+
+        }
+    }
+}
