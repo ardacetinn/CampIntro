873d1d965258a06055bb4e1813c992edac0fc277
ardac <ardacetinn2@gmail.com>
2023-02-05 20:54:23 +0300

    2.02.23

---
+++ OOP3/ApplicationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class ApplicationManager
+    {
+        //Dependency Injection ("Kredi ve log türünün soyut hali verilir
+        // somut hali kullanıcı isteğine bağlı olarak değişir.")
+        public void MakeApplication(ICreditManager creditManager, List<ILoggerService> loggerServices)
+        {
+            creditManager.Calculate();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+
+        public void MakeCreditInformation(List<ICreditManager> credits)
+        {
+            foreach (var credit in credits)
+            {
+                credit.Calculate();
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            ICreditManager personalFinanceCreditManager = new PersonalFinanceCreditManager();
+            ICreditManager vehicleCreditManager = new VehicleCreditManager();
+            ICreditManager houseCreditManager = new HouseCreditManager();
+            ICreditManager tradesmanCreditManager = new TradesmanCreditManager();
+            
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { databaseLoggerService, smsLoggerService };
+
+            ApplicationManager applicationManager = new ApplicationManager();
+            applicationManager.MakeApplication(tradesmanCreditManager, loggers);
+
+            List<ICreditManager> credits = new List<ICreditManager> { personalFinanceCreditManager, vehicleCreditManager };
+
+            //applicationManager.MakeCreditInformation(credits);
+
+        }
+    }
+}
